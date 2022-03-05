64b25ef1d78960dfdb22a3a15a070b37f1ad58ec
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-03-05 14:58:09 +0100

    ajout de classe en BEM

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 Rien à installer ici, il suffit d'ouvrir le fichier `index.html`.
 
+## Ressources
+
+- [Maquette sur Figma](https://www.figma.com/file/Q3yNeD7WTK9QHDldg9vaRl/UI-Design-FishEye-FR?node-id=0%3A1)
+- [exemples de datas en Json](https://github.com/OpenClassrooms-Student-Center/Front-End-Fisheye/blob/main/data/photographers.json) sur Github
+- [le design pattern Factory method](https://design-patterns.fr/fabrique)
+- [design pattern Factory](https://refactoring.guru/fr/design-patterns/factory-method)
+- [Outils d'accessibilité](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Accessibility#screenreaders) Ne pas hésiter de voir la page en Francais, il y a d'autres outils et exemples.
+- [la méthode fetch](https://fr.javascript.info/fetch)
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { city, country, id, price, name, portrait, tagline } = data;
+  // console.log(data);
+  const picture = `./assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const anchor = document.createElement("a");
+    const imgBox = document.createElement("div");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const boxDesc = document.createElement("div");
+    const localisation = document.createElement("h3");
+    const desc = document.createElement("p");
+    const seePrice = document.createElement("p");
+    imgBox.classList.add("box--img");
+    desc.classList.add("box_desc__desc");
+    seePrice.classList.add("box_desc__price");
+    boxDesc.classList.add("box_desc");
+    localisation.classList.add("box_desc__localite");
+    anchor.setAttribute("href", `photographer/${id}`);
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `photographe ${name}`);
+    h2.textContent = name;
+    localisation.textContent = `${city}, ${country}`;
+    desc.textContent = tagline;
+    seePrice.textContent = `${price}€/jour`;
+    article.appendChild(anchor);
+    anchor.appendChild(imgBox);
+    imgBox.appendChild(img);
+    anchor.appendChild(h2);
+    boxDesc.appendChild(localisation);
+    boxDesc.appendChild(desc);
+    boxDesc.appendChild(seePrice);
+    article.appendChild(boxDesc);
+    return article;
+  }
+  return { name, picture, id, getUserCardDOM };
 }

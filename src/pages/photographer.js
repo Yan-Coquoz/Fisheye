8fb6f4d8c1d8bcf9da5222ba4a0634661692944c
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-03-18 17:26:53 +0100

    blabla

--- src/pages/photographer.js
+++ src/pages/photographer.js
@@ -1,5 +1,53 @@
-//Mettre le code JavaScript lié à la page photographer.html
-async function displayPhotographer(user, media) {
-  const userDatas = [user, media];
-  console.log(userDatas);
+///Mettre le code JavaScript lié à la page photographer.html
+
+function extractUrlParam() {
+  // reccuperation de l'url courante.
+  const url = window.location.href;
+  const regex = /([^&=]+)=?([^&]*)/g;
+  //retourne les parametres de l'url sous forme de tableau
+  const getId = regex.exec(url);
+  return Number(getId[2]);
 }
+
+async function getPhotographerAndDatas() {
+  const photographers = await callApi();
+  return photographers.json();
+}
+
+async function displayPhotographerAndDatas(user, work) {
+  const photographHeader = document.querySelector(".photograph-header");
+  const photographer = new PhotographerFactory(
+    user.id,
+    user.name,
+    user.portrait,
+    user.city,
+    user.country,
+    user.price,
+    user.tagline
+  );
+  const cardPhotographer = photographer.getPhotographerDOM();
+  photographHeader.appendChild(cardPhotographer);
+}
+
+function getDatasByPhotographId(id, medias) {
+  const datas = medias.filter((media) => {
+    return media.photographerId === id;
+  });
+  return datas;
+}
+function getPhotographer(id, users) {
+  const datas = users.filter((user) => {
+    return user.id === id;
+  });
+  return datas[0];
+}
+async function rendu() {
+  const id = extractUrlParam();
+  const { photographers, media } = await getPhotographerAndDatas();
+  const datas = getDatasByPhotographId(id, media);
+  const photographer = getPhotographer(id, photographers);
+  console.log(photographer); // un objet
+  console.log(datas); // un tableau d'objets
+  displayPhotographerAndDatas(photographer, datas);
+}
+rendu();

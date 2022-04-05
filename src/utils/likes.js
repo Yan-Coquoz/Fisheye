53005727eaa044b120fe1a0138e715c44fce25d0
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-04-05 17:00:36 +0200

    accessibilité des likes au clavier

--- src/utils/likes.js
+++ src/utils/likes.js
@@ -1,8 +1,8 @@
 export function onLikes() {
-  let likeSelected = 0;
   //le footer contenant le nbr de likes
   const footerLikes = document.querySelector(".likes_container-footer > span");
   console.log(footerLikes);
+  // a la souris
   document.querySelectorAll(".likes_container").forEach((elt) => {
     elt.addEventListener("click", () => {
       // element séléctionné
@@ -27,6 +27,35 @@ export function onLikes() {
         currentHeartValue.textContent--;
 
         footerLikes.textContent = --currentNbrLike;
+      }
+    });
+  });
+  // au clavier
+  document.querySelectorAll(".likes_container").forEach((elt) => {
+    elt.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        const currentHeartValue = elt.querySelector(".likes_container >.likes");
+        const currentHeartIcon = elt.querySelector("span.fas.fa-heart");
+
+        currentHeartIcon.classList.toggle("active");
+        currentHeartValue.classList.toggle("active");
+        // nombre de like total
+        let currentNbrLike = Number(footerLikes.textContent);
+
+        if (
+          currentHeartIcon.classList.contains("active") &&
+          currentHeartValue.classList.contains("active")
+        ) {
+          currentHeartIcon.style.color = "#db8876";
+          currentHeartValue.textContent++;
+
+          footerLikes.textContent = ++currentNbrLike;
+        } else {
+          currentHeartIcon.style.color = "#901c1c";
+          currentHeartValue.textContent--;
+
+          footerLikes.textContent = --currentNbrLike;
+        }
       }
     });
   });
